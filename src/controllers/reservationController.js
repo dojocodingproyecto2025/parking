858f0ba475e6b8d646add82e0fffa6a9e55f6d32
alexander <alex18escorpio@gmail.com>
2025-04-22 23:20:34 -0500

    ADD: HU: PA-002: integration purcharse and reservation

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -8,3 +8,22 @@ export const createReservation = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const getReservations = async (req, res) => {
+    try {
+      const reservations = await reservationService.getReservations();
+      res.status(200).json(reservations);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+
+  export const deleteReservation = async (req, res, next) => {
+    try {
+      await reservationService.deleteReservation(req.params.id);
+      res.status(200).json({ message: 'Reservación eliminada exitosamente' });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
